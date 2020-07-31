59a6350b9f0a4b4ee7a343794d62766db55f571a
unknown <you@example.com>
2020-07-30 19:35:30 -0500

    added new js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // **THIS IS INCREDIBLY IMPORTANT THAT YOU DO BOTH SECTIONS!!! You will be doing only front-end work in 421 and you need to brush up on your HTML elements**
 
 
@@ -5,6 +7,7 @@
 //          PART ONE
 // ***************************
 // Write a JavaScript program to display the current day and time, start with:
+//NUMBER 1: Display Current Date
 console.log(new Date)
 
 const displayDate = () => {
@@ -12,15 +15,43 @@ const displayDate = () => {
 
   document.getElementById("display-element").innerHTML = currentDate;
 }
- 
+//Display Current Time
+// console.logCopy = console.log.bind(console);
+
+// console.log = function(data)
+// {
+//   var timestamp = '[' + Date.now() + ']';
+//   this.logCopy(timestamp, data);
+// };
+
+// console.log(new Time)
+
+// const displayTime = () => {
+//   const currentTime = getTime()
+
+//   document.getElementById("display-element").innerHTML = currentTime
+// }
+
 
 // Write a JavaScript program to convert a number to a string.
+//NUMBER 2
+// const MyInt1 = 7;
+// String MyString1 = Interger.toString(MyInt1);
 
+// console.log(MyString1);
 
 
 // Write a JavaScript program to convert a string to the number.
+//NUMBER 3
+console.log("input:", someString){
+console.log("type of input: ", typeof someString)
+const number = '22714'
 
+console.log(parseInt(number, 10) + 1);
 
+console.log("type of converted:", typeof num);
+console.log("converted:", num);
+}
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   // * Boolean
@@ -33,12 +64,32 @@ const displayDate = () => {
 
   
 // Write a JavaScript program that adds 2 numbers together.
+//NUMBER 5
+//I tested in Repl.it and the sum came back correct, 33
+function sumOfTwoNumbers(a, b){
+  const sum = a + b;
 
+  return sum
+}
+
+sumOfTwoNumbers(10, 23)
 
 
 // Write a JavaScript program that runs only when 2 things are true.
+//NUMBER 6
+//In the Conditionals prework video, I didn't understand why he changed his inputs to  if (inputX && inputY); Why did he have to do that in order for the function to work?
+const input1 = 200 //truthy
+const input2 = 10 //truthy
 
+const myFunc = (input1, input) => {
+  if (input1 && input2){
+    return true
+  } else {
+    return false
+    }
+  }
 
+  myFunc(input1, input2)
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
